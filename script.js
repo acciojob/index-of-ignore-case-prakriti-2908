e8559a0e801d2805684786196bb8bb0d6fad66d8
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-15 16:55:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-function indexOfIgnoreCase(s1, s2) {
+function indexOfIgnoreCase(S1, S2) {
   // write your code here
+  let ans = -1;
+  let s1 = S1.toLowerCase();
+  let s2 = S2.toLowerCase();
+  for(let i=0;i<s1.length;i++){
+      if(s2[0]==s1[i] ){
+          let tr = -1;
+          let a = i;
+          for(let j=0;j<s2.length;j++){
+              if(s2[j]==s1[a]){
+                  a++;
+                  tr = 1;
+              }else if(s2[j]!=s1[i]){
+                  tr = -1;
+                  break;
+              }
+          }
+          if(tr!=-1){
+              return i;
+          }
+      }
+  }
+  return -1
 }
 
 // Please do not change the code below
